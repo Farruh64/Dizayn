3eaf781940e23c48661764fb6b9e24de33072c82
Farruh64 <rixsiboyevfaruuh@gmail.com>
2022-02-15 17:18:19 +0300

    s2

--- academiya/settings.py
+++ academiya/settings.py
@@ -86,9 +86,10 @@ WSGI_APPLICATION = 'academiya.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
+
 
 
 # Password validation
